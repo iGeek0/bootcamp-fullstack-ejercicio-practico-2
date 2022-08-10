2adea0749ff2e99d9d8a2ee1d68740d492ea642b
Jesus Cardenas <jesus.cardenas@envia.com>
2022-08-09 21:54:59 -0500

    corte ejercicio

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+console.log("Entro a main.js");
+
+const paises = ["Argentina", "Brasil", "Chile", "Colombia", "Ecuador", "Peru", "Uruguay", "Venezuela"];
+
+const animales = ["Perro", "Gato", "Pájaro", "Conejo", "Caballo", "Oso", "Jirafa", "Vaca", "Cocodrilo", "Ballena", "Conejo", "Gato", "Perro", "Pájaro", "Conejo", "Caballo", "Oso", "Jirafa", "Vaca", "Cocodrilo", "Ballena"];
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const nuevoArrayNumeros = [];
+
+
+
+
+
